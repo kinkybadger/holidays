2d06706e7a518ba82fc166194321a359e39ead76
Leo Theobald <leotheo@gmail.com>
2016-07-17 14:14:30 +0100

    refactored some vars plus hid unused items

--- application.js
+++ application.js
@@ -7,16 +7,16 @@ function Holidays() {
   var countryData = [];
   this.countryUrl = 'tours.json';
 
-  this.setUrl = function(url) {
-    this.countryUrl = url;
-  };
+  // this.setUrl = function(url) {
+  //   this.countryUrl = url;
+  // };
 
   // Helpers.
   this.getCountries = function() {
     // Get the json.
     $.getJSON(this.countryUrl, function() {})
     // Retrieval.
-    .done(function(data, status) {
+    .done(function(data) {
       countryData = data;
       var countries = $('<ul id="country"></ul>');
       // loop and add as 'li a' to ul#country.
@@ -25,18 +25,19 @@ function Holidays() {
       }
 
       $('#country').detach().html(countries).appendTo('.menu');
-      $('h2.country').html(data[0].country).prepend('.tour');
+      $('h2.country').html(data[i].country).prepend('.tour');
     })
     // If Error, report.
     .fail(function(request, errorType, errorMessage) {
       console.log("Request by: " + request + " error: " + errorType + " = " + errorMessage);
     })
-  }
+  };
 
   this.loadCitiesByCountry = function() {
-    var id = $(this).attr('id');
-    var cityList = $('.tour');
-    var markUp =  cityList.html('');
+    var id = $(this).attr('id'),
+        cityList = $('.tour'),
+        markUp =  cityList.html('');
+
     for(i in countryData[id].cities || i in countryData[id].photo) {
       markUp.append('<div class="city" data-location="' + countryData[id].cities[i].name + '">'
         + '<h3 class="name">' + countryData[id].cities[i].name + '</h3>'
@@ -48,9 +49,10 @@ function Holidays() {
     // Not pretty, but works!
     $("<img src="+countryData[id].photo[0].src+countryData[id].photo[0].image+" />").prependTo(cityList);
     $('<h2 class="country">'+ countryData[id].country + '</h2>').prependTo(cityList);
+
     // Display the result for the clicked item.
     markUp.append(markUp).fadeIn();
-  }
+  };
 
   //this.loadPhotosFromCity = function() {
   //
@@ -80,7 +82,3 @@ function logIt(text) {
   console.log(text);
 }
 
-// Date time.
-function getTime() {
-  $('#date').append(currentTime);
-}
