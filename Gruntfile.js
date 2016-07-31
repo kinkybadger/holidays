b39eaa6e6b8c45b2510a4186a836ec836bd5535c
leo theobald <leotheo@gmail.com>
2016-07-31 14:14:13 +0100

    added save dev version of grunt http server

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     'http-server': {
       'dev': {
         // the server root directory
-        root: <path>,
+        root: '/html/less-work/holidays/',
 
         // the server port
         // can also be written as a function, e.g.
@@ -16,9 +16,9 @@ module.exports = function(grunt) {
         // If specified to, for example, "127.0.0.1" the server will
         // only be available on that ip.
         // Specify "0.0.0.0" to be available everywhere
-        host: "0.0.0.0",
+        host: "127.0.0.1",
 
-        cache: <sec>,
+        cache: 60,
         showDir : true,
         autoIndex: true,
 
@@ -37,14 +37,14 @@ module.exports = function(grunt) {
         proxy: "http://someurl.com",
 
         /// Use 'https: true' for default module SSL configuration
-        /// (default state is disabled)
-        https: {
-            cert: "cert.pem",
-            key : "key.pem"
-        },
+        /// (default state is disabled)P
+        // https: {
+        //     cert: "cert.pem",
+        //     key : "key.pem"
+        // },
 
         // Tell grunt task to open the browser
-        openBrowser : false,
+        openBrowser : true,
 
         // customize url to serve specific pages
         customPages: {
@@ -75,6 +75,6 @@ module.exports = function(grunt) {
       }
     }
   });
-
+  grunt.loadNpmTasks('grunt-http-server');
   grunt.registerTask('default', ['less', 'watch']);
 };
